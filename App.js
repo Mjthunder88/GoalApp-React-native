11f56cfc154000c011f6539ef1b5add037aa629e
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-04-16 15:36:39 -0600

    got the list to work

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
-
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
+  const [goalEntered, setGoalEntered] = useState('')
+  const [goals, setGoals] = useState([])
+
+  let goalInputHandler = (enteredTxt) => {
+    setGoalEntered(enteredTxt)
+  }
+
+  let addGoalHandler = () => {
+    setGoals((goals) => {
+      return [...goals, goalEntered]
+    })
+  }
+
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder='Your Course Goal!'></TextInput>
-        <Button title='Add Goal' />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your Course Goal!' onChangeText={goalInputHandler}></TextInput>
+        <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
-      <View>
-        <Text>List of Goals</Text>
+      <View style={styles.goalsContainer}>
+      <ScrollView >
+        {goals.map((goal, index) => (
+          <View key={index} style={styles.goalItem}>
+            <Text style={styles.goalTxt}>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
       </View>
     </View>
   );
@@ -18,15 +37,38 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 10,
-    padding: 50,
+    paddingHorizontal: 16,
+    paddingTop: 50,
   },
-  coolStyle: {
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
+  },
+  textInput: {
+    borderWidth: 1,
+    width: '70%',
+    borderColor: 'black',
     padding: 5,
-    borderWidth: 3,
-    borderColor: 'black'
+    marginRight: 5,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: 'black',
+  },
+  goalTxt: {
+    color: 'white',
   }
 });
+
+// * Make sure you put the correct styling on the elements you want to target sense REACT NATIVE does not have styling inheritance/ Cascading
+
